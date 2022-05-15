20fae1a7ada693591c60586bb6a194e4b62039c8
agentmatti <48174418+agentmatti@users.noreply.github.com>
2022-05-15 12:44:18 +0200

    finished the tutorial
    jump not working
    enemy + test level added

---
+++ Game/players/Actors/actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var player_speed: = Vector2(100.0, 1000.0)
+export var enemy1_speed: = Vector2(40.0, 1000.0)
+export var gravity: = 4000.0
+var _velocity: = Vector2.ZERO
+
