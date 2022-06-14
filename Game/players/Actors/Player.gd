8703f37e08b264d2be32053c7734ef520df5dab9
agentmatti <48174418+agentmatti@users.noreply.github.com>
2022-06-14 19:42:57 +0200

    starters
    created the scene for an enemy spawner
    started on the dash for the player

--- Game/players/Actors/Player.gd
+++ Game/players/Actors/Player.gd
@@ -2,6 +2,8 @@ extends Actor
 export var stomp_impulse: = 500.0
 export var lives: = 3
 
+export onready var dash: = 0.0
+
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 onready var Label: Label = get_node("Label")
 
@@ -47,6 +49,13 @@ func get_direction() -> Vector2:
 	)
 	
 
+func dash():
+	if Input.is_action_just_pressed("dash"):
+		dash = 1000.0
+	else:
+		dash = 0.0
+	return dash
+
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
@@ -54,7 +63,7 @@ func calculate_move_velocity(
 		is_jump_interruptet: bool
 	) -> Vector2:
 	var out: = linear_velocity
-	out.x = player_speed.x * direction.x
+	out.x = player_speed.x * direction.x + dash
 	out.y = out.y + (gravity * get_physics_process_delta_time())
 	if direction.y == -1.0:
 		out.y = player_speed.y * direction.y
--- Game/players/Actors/actor.gd
+++ Game/players/Actors/actor.gd
@@ -3,8 +3,8 @@ class_name Actor
 
 const FLOOR_NORMAL: = Vector2.UP
 
-export var player_speed: = Vector2(100.0, 1000.0)
+export var player_speed: = Vector2(100.0, 400.0)
 export var enemy1_speed: = Vector2(40.0, 1000.0)
-export var gravity: = 4000.0
+export var gravity: = 2000.0
 var _velocity: = Vector2.ZERO
 
